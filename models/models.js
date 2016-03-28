eb732760d7f6342fdcb201676b35848e58cfb070
Ben Stockwell <ben@scrupulo.com>
2016-03-28 00:19:50 -0400

    error checking on db

--- models/models.js
+++ models/models.js
@@ -7,8 +7,8 @@ var Sequelize = require('sequelize'),
 // app/model/models.js
 var message_schema = {
 		id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
-		title: { type: Sequelize.STRING, allowNull: false },
-		body: { type: Sequelize.STRING, allowNull: false }
+		title: { type: Sequelize.STRING, validate: {len: [1,100]}},
+		body: { type: Sequelize.STRING, allowNull: false, validate: {len: [1,255]}}
 	};
 	
 var Message = connection.define('messages', message_schema, opts);
